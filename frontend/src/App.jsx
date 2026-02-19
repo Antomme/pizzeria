d698684987921415729fa603aed8ca0b123d1e6b
Tomme <anita.tomme@smartindustrycampus.d>
2026-02-19 17:06:01 +0100

    Timeout hinzugefügt und Optische Anpassungen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import WelcomePage from './pages/welcomePage';
 import OrderPage from './pages/orderPage';
@@ -7,10 +7,22 @@ import SummaryPage from './pages/summaryPage';
 export default function App() {
   const [order, setOrder] = useState({}); // z.B. { "Margherita": 2 }
 
+  // Auto-Reset nach X Sekunden (z.B. 60s)
+  useEffect(() => {
+    if (Object.keys(order).length === 0) return; // nichts zu resetten
+
+    const timeout = setTimeout(() => {
+      setOrder({});
+      console.log("Order auto-reset after timeout!");
+    }, 60000*15); // 60000ms = 60 Sekunden
+
+    return () => clearTimeout(timeout); // Timer aufräumen bei Änderungen oder unmount
+  }, [order]);
+
   const router = createBrowserRouter([
     { path: "/", element: <WelcomePage /> },
     { path: "/order", element: <OrderPage order={order} setOrder={setOrder} /> },
-    { path: "/summary", element: <SummaryPage order={order} /> } // <-- hier übergeben!
+    { path: "/summary", element: <SummaryPage order={order} /> }
   ]);
 
   return <RouterProvider router={router} />;
